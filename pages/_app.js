e2224d815c1ce2af27c401a541ea5b4278cecdea
Akshal Dhal <hackproofworld@gmail.com>
2022-01-26 20:54:01 +0530

    testing branch (added firebase configs)

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,37 @@ import '../styles/globals.css'
 import '../styles/custom.css'
 import Link from 'next/link'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue } = require('firebase-admin/firestore');
+
+initializeApp();
+
+const db = getFirestore();
+
+
 function MyApp({ Component, pageProps }) {
   return (
     <div>
