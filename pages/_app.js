fe37cd507f264dad117536afba77797e0f563642
Akshal Dhal <hackproofworld@gmail.com>
2022-02-06 01:41:13 +0530

    added barebones pages

--- pages/_app.js
+++ pages/_app.js
@@ -6,8 +6,33 @@ import Link from 'next/link'
 function MyApp({ Component, pageProps }) {
     return (
       <div>
-        <Component {...pageProps} />
-      </div>
+      <nav className="border-b p-6 bg-slate-700">
+        <p className="text-4xl font-bold text-sky-100">Frosty Marketplace</p>
+        <div className="flex mt-4">
+          <Link href="/">
+            <a className="mr-4 text-sky-400">
+              Home
+            </a>
+          </Link>
+          <Link href="/create-item">
+            <a className="mr-6 text-sky-400">
+              Sell Digital Asset
+            </a>
+          </Link>
+          <Link href="/my-assets">
+            <a className="mr-6 text-sky-400">
+              My Digital Assets
+            </a>
+          </Link>
+          <Link href="/creator-dashboard">
+            <a className="mr-6 text-sky-400">
+              Creator Dashboard
+            </a>
+          </Link>
+        </div>
+      </nav>
+      <Component {...pageProps} />
+    </div>
     )
   }
   
