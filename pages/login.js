9d2ab7b7c49831a929b5536154de8c80604b6139
Akshal Dhal <hackproofworld@gmail.com>
2022-02-18 09:38:36 +0530

    added login logout buttons and display logged in address

--- pages/login.js
+++ pages/login.js
@@ -4,14 +4,43 @@ import Web3 from 'web3'
 import { useEffect, useState } from 'react'
 import Head from 'next/head'
 import { result } from 'lodash'
-
-
+import { Moralis } from 'moralis'
 
 export default function userManage() {
-//////////////////////////////////////////////////
-    const network = "rinkeby"
-//////////////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////
+    const appId = "vdgEX5APUMifU8c5dhB5F8Xm3bb8X5uGEa0LZRxf";
+    const serverUrl = "https://6na6pzj82wfu.usemoralis.com:2053/server";
+    //////////////////////////////////////////////////////////////////////
+
+    const [UserId, setUserId] = useState('')
+
+
+    async function GetUser() {
+        setUserId(Moralis.User.current().attributes.ethAddress);
+    }
+
+    async function login() {
+        let user = Moralis.User.current();
+        if (!user) {
+          user = await Moralis.authenticate();
+        }
+        GetUser();
+      }
+
+      async function logOut() {
+        await Moralis.User.logOut();
+        setUserId('')
+      }
+
+      useEffect(() => {
+        Moralis.start({ serverUrl, appId });
+        GetUser()
+      }, [])
+      
     return (
-    <div><button>abc</button></div>
+    <div><button onClick={login}>login</button>
+    <button onClick={logOut}>logout</button>
+    <p>{UserId}</p>
+    </div>
     )
 }
